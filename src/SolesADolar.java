608089c0cc24760be03d27a933608814f19c60be
SandroV09 <sandro.valderrama09@gmail.com>
2024-12-28 19:39:28 -0500

    first commit

---
+++ src/SolesADolar.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SolesADolar {
+
+    public void SolesADolar() throws IOException, InterruptedException {
+        // Implementación de la conversión de soles a dólares
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el valor que desea convertir:");
+        double cantidad = scanner.nextDouble();
+
+        Conversor conversor = new Conversor();
+
+        System.out.println("El valor de " + cantidad + " DOLARES, " + "corresponde al valor final de: "
+                + "S/." + conversor.conversor("USD", "PEN", cantidad) + " PEN");
+    }
+}
